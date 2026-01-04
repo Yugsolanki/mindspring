8170d7246bea4441d6ea94bbf353c278c1d40c1c
Yug Solanki <yugsolanki15@gmail.com>
2026-01-04 18:37:38 +0530

    refactor: name changed to ScrapedContent and ScrapedResource

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,6 @@
 from app.models.website import Website
-from app.models.resource import ScrapedResource
-from app.models.content import ScrapedContent
+from app.models.scraped_resource import ScrapedResource
+from app.models.scraped_content import ScrapedContent
 from app.core.database import Base
 
 # This allows you to just do: from app.models import Website, ScrapedResource
